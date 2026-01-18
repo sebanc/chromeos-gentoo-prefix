1c68c4f8dc652dbd9e58e506de1215f301bfb9d9
sebanc <sebanc.xda@gmail.com>
2026-01-18 09:14:58 +0100

    Update previous prefix instead of rebuilding

--- build.sh
+++ build.sh
@@ -3,8 +3,6 @@
 rm -rf ./chroot
 mkdir ./chroot
 
-if [ -z ${1} ]; then stage=stage1; else stage="${1}"; fi
-
 if [ -f ../chromiumos-stage3/chromiumos_stage3.tar.gz ]; then
 	echo "Using local ChromiumOS Stage3"
 	cp ../chromiumos-stage3/chromiumos_stage3.tar.gz ./chromiumos_stage3.tar.gz
@@ -24,7 +22,7 @@ mount --bind /dev/pts ./chroot/dev/pts
 mount --make-slave ./chroot/dev/pts
 mount -t tmpfs -o mode=1777 none ./chroot/dev/shm
 
-cp ./${stage} ./chroot/init
+cp ./update ./chroot/init
 if [ -f ./chromeos_gentoo_prefix.tar.gz ]; then
 	mkdir -p ./chroot/usr/local
 	tar zxf ./chromeos_gentoo_prefix.tar.gz -C ./chroot/usr/local
@@ -57,10 +55,9 @@ if mountpoint -q ./chroot/sys; then umount ./chroot/sys; fi
 if mountpoint -q ./chroot/proc; then umount ./chroot/proc; fi
 
 if [ -f ./chroot/usr/local/.finished ]; then
-	if [ "${stage}" == "stage1" ]; then suffix="_mini"; fi
-	rm -f ./chroot/usr/local/.finished ./chromeos_gentoo_prefix${suffix}.tar.gz ./chromeos_gentoo_prefix${suffix}_"$(date +"%Y%m%d")".tar.gz
-	tar zcf ./chromeos_gentoo_prefix${suffix}_"$(date +"%Y%m%d")".tar.gz -C ./chroot/usr/local .
-	ln -s ./chromeos_gentoo_prefix${suffix}_"$(date +"%Y%m%d")".tar.gz ./chromeos_gentoo_prefix${suffix}.tar.gz
-	chown ${SUDO_UID}:$(id -g ${SUDO_UID}) ./chromeos_gentoo_prefix${suffix}.tar.gz ./chromeos_gentoo_prefix${suffix}_"$(date +"%Y%m%d")".tar.gz
+	rm -f ./chroot/usr/local/.finished ./chromeos_gentoo_prefix.tar.gz ./chromeos_gentoo_prefix_"$(date +"%Y%m%d")".tar.gz
+	tar zcf ./chromeos_gentoo_prefix_"$(date +"%Y%m%d")".tar.gz -C ./chroot/usr/local .
+	ln -s ./chromeos_gentoo_prefix_"$(date +"%Y%m%d")".tar.gz ./chromeos_gentoo_prefix.tar.gz
+	chown ${SUDO_UID}:$(id -g ${SUDO_UID}) ./chromeos_gentoo_prefix.tar.gz ./chromeos_gentoo_prefix_"$(date +"%Y%m%d")".tar.gz
 fi
 
